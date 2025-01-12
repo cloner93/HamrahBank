5ba9c582c11728f9d75a4b6fe089a2a3b8dcd3b2
mehdizadeh_ma <mehdizadeh7250@gmail.com>
2025-01-12 15:32:03 +0330

    add profile business login in profile module(dependencies, injection modules, repos, viewModel, viewState and ...)

---
+++ profile/src/main/java/com/pmb/profile/presentaion/profile/viewModel/ProfileViewActions.kt
@@ -0,0 +1,8 @@
+package com.pmb.profile.presentaion.profile.viewModel
+
+import com.pmb.core.platform.BaseViewAction
+
+sealed interface ProfileViewActions :BaseViewAction {
+    data object ClearAlert : ProfileViewActions
+    data object LogoutAccount :ProfileViewActions
+}
