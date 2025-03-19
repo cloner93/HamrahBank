7e6fa799e170683c4339880d0632199df6b53ca9
cloner93 <targholi2112@gmail.com>
2025-03-20 00:42:43 +0330

    feat: add pie chart for showing deposits
    This commit introduces a new pie chart feature for displaying deposit information and adds a new screen for showing balance and deposits.
    
    - Adds a `PieChart` composable for visualizing deposit distribution.
    - Creates a `BalanceScreen` composable to display total balance and a list of deposits.
    - Adds a `DepositRow` composable for rendering individual deposit items.
    - Adds `DepositsChartModel` data class for representing deposit data.
    - Creates a `BalanceViewModel` to fetch and manage deposit data.
    - Implement mapping function `mapToDepositsChartModel` for converting `DepositModel` to `DepositsChartModel`.
    - Implement `generateColorByIndex` for generate different color by index.
    - Implements logic for calculating and displaying total balance.
    - Implements `loadDeposits` function for loading list of deposits.

---
+++ account/src/main/java/com/pmb/account/presentation/balance/BalanceViewModel.kt
@@ -0,0 +1,91 @@
+package com.pmb.account.presentation.balance
+
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewModelScope
+import com.pmb.account.presentation.component.DepositModel
+import com.pmb.account.usecase.deposits.GetDepositsUseCase
+import com.pmb.core.platform.BaseViewAction
+import com.pmb.core.platform.BaseViewEvent
+import com.pmb.core.platform.BaseViewModel
+import com.pmb.core.platform.BaseViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BalanceViewModel @Inject constructor(
+    initialState: BalanceViewState,
+    private val getDepositsUseCase: GetDepositsUseCase,
+) : BaseViewModel<BalanceViewActions, BalanceViewState, BalanceViewEvents>(initialState) {
+    override fun handle(action: BalanceViewActions) {
+        when (action) {
+
+            else -> {}
+        }
+    }
+
+    init {
+        loadDeposits()
+    }
+
+    private fun loadDeposits() {
+        viewModelScope.launch {
+            setState { it.copy(isLoading = true) }
+
+            try {
+                val listOfDeposits: List<DepositsChartModel> = getDepositsUseCase().mapToDepositsChartModel()
+                val balance = listOfDeposits.sumOf { deposit -> deposit.amount }
+
+                setState {
+                    it.copy(
+                        deposits = listOfDeposits,
+                        totalBalance = balance,
+                        isLoading = false
+                    )
+                }
+
+            } catch (e: Exception) {
+                setState {
+                    it.copy(
+                        errorMessage = e.message ?: "Failed to load deposits",
+                        isLoading = false
+                    )
+                }
+                postEvent(BalanceViewEvents.ShowError(e.message ?: "Failed to load deposits"))
+            }
+        }
+    }
+
+}
+
+
+fun List<DepositModel>.mapToDepositsChartModel(): MutableList<DepositsChartModel> {
+    val totalSum = this.sumOf { it.amount }
+    return this.mapIndexed { index, it ->
+        DepositsChartModel(
+            value = (it.amount * 100 / totalSum).toFloat(),
+            color = generateColorByIndex(index),
+            title = it.title,
+            depositNumber = it.depositNumber,
+            amount = it.amount,
+            currency = it.currency
+        )
+    }.toMutableList()
+}
+
+fun generateColorByIndex(index: Int): Color {
+    val hue = (index * 137.508f) % 360
+    return Color.hsv(hue, 0.7f, 0.9f)
+}
+
+sealed interface BalanceViewEvents : BaseViewEvent {
+    data class ShowError(val message: String) : BalanceViewEvents
+}
+
+sealed interface BalanceViewActions : BaseViewAction {}
+data class BalanceViewState(
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val deposits: List<DepositsChartModel> = listOf(),
+    val totalBalance: Double = 0.0,
+) : BaseViewState
