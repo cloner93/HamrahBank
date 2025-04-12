a66a65b4922ed9c5874e9b6d38cd61170c2f444a
targholi <targholi@behsazan.net>
2025-04-12 15:20:37 +0330

    feat: use 'scrollState = null' in HomeScreen.

--- home/src/main/java/com/pmb/home/presentation/home/HomeScreen.kt
+++ home/src/main/java/com/pmb/home/presentation/home/HomeScreen.kt
@@ -41,7 +41,7 @@ fun HomeScreen(
     val viewState by viewModel.viewState.collectAsState()
     AppContent(
         modifier = Modifier.padding(horizontal = 16.dp),
-        requiredVerticalScroll = false,
+        scrollState = null,
         topBar = {
             Box(
                 modifier = Modifier
