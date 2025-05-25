70e97566786653cfa1e15cc7a109204226bffbff
targholi <targholi@behsazan.net>
2025-05-25 11:27:48 +0330

    Refactor: Inject DataStore into ThemeRepositoryImpl
    Instead of creating a DataStore instance within `ThemeRepositoryImpl`, this change injects `DataStore<Preferences>` directly.
    
    A new `DataStoreModule` is introduced to provide the singleton `DataStore<Preferences>` instance.

--- app/src/main/java/com/pmb/mobile/data/theme/ThemeRepositoryImpl.kt
+++ app/src/main/java/com/pmb/mobile/data/theme/ThemeRepositoryImpl.kt
@@ -1,30 +1,29 @@
 package com.pmb.mobile.data.theme
 
-import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.pmb.core.platform.ThemeMode
 import com.pmb.mobile.domain.theme.repository.ThemeRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ThemeRepositoryImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val dataStore: DataStore<Preferences>
 ) : ThemeRepository {
-    private val Context.dataStore by preferencesDataStore("settings")
+
     private val THEME_KEY = stringPreferencesKey("theme_mode")
 
     override suspend fun saveThemeMode(mode: ThemeMode) {
-        context.dataStore.edit { prefs ->
+        dataStore.edit { prefs ->
             prefs[THEME_KEY] = mode.name
         }
     }
 
     override fun getThemeModeFlow(): Flow<ThemeMode> {
-        return context.dataStore.data.map { prefs ->
+        return dataStore.data.map { prefs ->
             val theme = prefs[THEME_KEY] ?: ThemeMode.SYSTEM.name
             ThemeMode.valueOf(theme)
         }
