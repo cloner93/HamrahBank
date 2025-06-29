4a1d0b5a2714ad9f2ce41e5b0d1640e077eec527
targholi <targholi@behsazan.net>
2025-06-29 10:45:17 +0330

    feat: config network security in manifest

--- network/src/main/java/com/pmb/network/SslSetting.kt
+++ network/src/main/java/com/pmb/network/SslSetting.kt
@@ -1,35 +1,37 @@
 package com.pmb.network
 
-import java.io.FileInputStream
+import android.content.Context
 import java.security.KeyStore
+import java.security.cert.CertificateFactory
 import javax.net.ssl.SSLContext
 import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
 @Deprecated("This method is temporary. use real ssl pinning instead")
 object SslSettings {
-    fun getKeyStore(): KeyStore {
-        val keyStoreFile = FileInputStream("keystore.jks")
-        val keyStorePassword = "foobar".toCharArray()
-        val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
-        keyStore.load(keyStoreFile, keyStorePassword)
-        return keyStore
+    fun getTrustManagerFactory(context: Context): TrustManagerFactory {
+        val certificate = context.assets.open("crt/cert.cer").use { inputStream ->
+            CertificateFactory.getInstance("X.509").generateCertificate(inputStream)
+        }
+
+        val keyStore = KeyStore.getInstance(KeyStore.getDefaultType()).apply {
+            load(null, null)
+            setCertificateEntry("ca", certificate)
+        }
+
+        return TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply {
+            init(keyStore)
+        }
     }
 
-    fun getTrustManagerFactory(): TrustManagerFactory? {
-        val trustManagerFactory =
-            TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-        trustManagerFactory.init(getKeyStore())
-        return trustManagerFactory
+    fun getTrustManager(context: Context): X509TrustManager {
+        val trustManagers = getTrustManagerFactory(context).trustManagers
+        return trustManagers.first { it is X509TrustManager } as X509TrustManager
     }
 
-    fun getSslContext(): SSLContext? {
+    fun getSslContext(context: Context): SSLContext {
         val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, getTrustManagerFactory()?.trustManagers, null)
+        sslContext.init(null, getTrustManagerFactory(context).trustManagers, null)
         return sslContext
-    }
-
-    fun getTrustManager(): X509TrustManager {
-        return getTrustManagerFactory()?.trustManagers?.first { it is X509TrustManager } as X509TrustManager
     }
 }
