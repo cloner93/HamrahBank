5134b31b7df02a3a3c46cc38a558f5fbdff594a4
Masoud Sarabadani <codewithkael@gmail.com>
2025-07-20 11:34:19 +0330

    update base64 helper

--- core/src/main/java/com/pmb/core/utils/Base64Helper.kt
+++ core/src/main/java/com/pmb/core/utils/Base64Helper.kt
@@ -3,21 +3,21 @@ package com.pmb.core.utils
 import android.content.Context
 import android.net.Uri
 import android.util.Base64
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.FileOutputStream
 import java.nio.charset.StandardCharsets
 
 object Base64FileHelper {
-    suspend fun encodeToBase64(input: File): String = withContext(Dispatchers.IO) {
+
+    fun encodeToBase64(input: File, scope: CoroutineScope): Deferred<String> = scope.async(Dispatchers.IO) {
         val bytes = input.readBytes()
         Base64.encodeToString(bytes, Base64.NO_WRAP)
             .toByteArray(StandardCharsets.UTF_8)
             .toString(StandardCharsets.UTF_8)
     }
 
-    suspend fun encodeToBase64(context: Context, input: Uri): String = withContext(Dispatchers.IO) {
+    fun encodeToBase64(context: Context, input: Uri, scope: CoroutineScope): Deferred<String> = scope.async(Dispatchers.IO) {
         val inputStream = context.contentResolver.openInputStream(input)
             ?: throw IllegalArgumentException("Cannot open URI: $input")
         val bytes = inputStream.use { it.readBytes() }
@@ -26,7 +26,7 @@ object Base64FileHelper {
             .toString(StandardCharsets.UTF_8)
     }
 
-    suspend fun decodeFromBase64(base64Utf8String: String, outputFile: File): File = withContext(Dispatchers.IO) {
+    fun decodeFromBase64(base64Utf8String: String, outputFile: File, scope: CoroutineScope): Deferred<File> = scope.async(Dispatchers.IO) {
         val decodedBytes = Base64.decode(
             base64Utf8String.toByteArray(StandardCharsets.UTF_8),
             Base64.NO_WRAP
