f210ad254433e392ed70104bb37bbcb320be8f5b
Ali Mehdizadeh <mehdizadeh_ma@behsazan.net>
2025-07-28 12:19:30 +0330

    fix some bugs in login

--- feature/auth/src/main/java/com/pmb/auth/presentation/first_login_confirm/FirstLoginConfirmScreen.kt
+++ feature/auth/src/main/java/com/pmb/auth/presentation/first_login_confirm/FirstLoginConfirmScreen.kt
@@ -125,7 +125,6 @@ fun FirstLoginConfirmScreen(
                 if (otp.length <= 6) otp = it
             },
         )
-
         Spacer(modifier = Modifier.size(32.dp))
         AppButton(
             modifier = Modifier.fillMaxWidth(),
