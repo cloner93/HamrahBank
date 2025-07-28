9a16c58e4bf3837fd8e6475d21ee0e3e372968f8
pooriak <pouriyakhalaj@gmail.com>
2025-07-28 18:02:19 +0330

    Refactor: Enhance transfer feature with UI and logic improvements
    This commit introduces several refinements to the transfer feature:
    
    - **UI Enhancements:**
        - Increased `profileSize` and `iconSize` in `ClientBankProfileInfo` for better visual presentation.
        - Conditionally display client name in `ClientBankProfileInfo` only if it's not empty.
        - Improved layout of `MenuItem` by adjusting weights and using `Row` for better alignment of start/end content.
        - Updated password visibility icon in `AppPasswordTextField` to reflect the correct state.
        - Added a new icon `ic_mellat_to_mellat.xml` for Mellat-to-Mellat transfers.
        - Used the new icon in `TransferMethodScreen` for Mellat-to-Mellat payment type.
    
    - **Logic Improvements:**
        - Modified `getBankByAccountNumber` in `BankUtil` to specifically check for Mellat bank accounts.
        - Updated `validateAccountNumber` in `BankUtil` to use the new Mellat bank account validation and adjust `maxLength`.
        - Refined `detectBankIdentifierNumberType` in `BankUtil` to use the new Mellat bank account validation.
        - Changed `extractAllLongNumbers` in `BankUtil` to have a default `minLength` of 5.
        - Implemented `validateMellatBankAccount` function in `BankUtil` for specific Mellat account validation.
        - In `AccountDetailUseCase`, clear "IR" prefix from IBAN before fetching account details.
        - Standardized IBAN format in `ClientBankEntity` to always include "IR" prefix.
    
    - **Code Cleanup:**
        - Removed unused import `kotlin.invoke` in `TransferDestinationInputViewModel`.

--- feature/transfer/src/main/java/com/pmb/transfer/domain/use_case/AccountDetailUseCase.kt
+++ feature/transfer/src/main/java/com/pmb/transfer/domain/use_case/AccountDetailUseCase.kt
@@ -2,6 +2,7 @@ package com.pmb.transfer.domain.use_case
 
 import com.pmb.core.platform.BaseUseCase
 import com.pmb.core.platform.Result
+import com.pmb.transfer.domain.entity.BankIdentifierNumberType
 import com.pmb.transfer.domain.entity.TransactionClientBankEntity
 import com.pmb.transfer.domain.param.AccountDetailParam
 import com.pmb.transfer.domain.repository.TransferRepository
@@ -12,6 +13,10 @@ class AccountDetailUseCase @Inject constructor(
     private val transferRepository: TransferRepository
 ) : BaseUseCase<AccountDetailParam, TransactionClientBankEntity>() {
     override suspend fun execute(params: AccountDetailParam): Flow<Result<TransactionClientBankEntity>> {
-        return transferRepository.fetchAccountDetail(params)
+        val clearId = when (params.type) {
+            BankIdentifierNumberType.IBAN -> params.value.uppercase().replaceFirst("IR", "")
+            else -> params.value
+        }
+        return transferRepository.fetchAccountDetail(params.copy(value = clearId))
     }
 }
