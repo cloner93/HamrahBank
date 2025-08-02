de4262fa7ef072408a5890af49e36a56db6cbf8e
targholi <targholi@behsazan.net>
2025-08-02 14:44:25 +0330

    feat: add biometric data store
    This commit introduces a new `BiometricDataStore` to manage biometric authentication state.
    
    Key changes:
    - Created `BiometricDataStore` interface with methods to get and set biometric state.
    - Implemented `BiometricDataStoreImpl` using AndroidX DataStore for persistent storage.
    - Integrated `BiometricDataStore` into `LocalServiceProvider` to make it accessible throughout the data layer.

--- data/src/main/java/com/pmb/data/serviceProvider/local/LocalServiceProviderImpl.kt
+++ data/src/main/java/com/pmb/data/serviceProvider/local/LocalServiceProviderImpl.kt
@@ -1,12 +1,18 @@
 package com.pmb.data.serviceProvider.local
 
+import com.pmb.data.serviceProvider.local.biometric.BiometricDataStore
 import com.pmb.data.serviceProvider.local.user.UserDataStore
 import javax.inject.Inject
 
 class LocalServiceProviderImpl @Inject constructor(
-    private val userDataStore: UserDataStore
+    private val userDataStore: UserDataStore,
+    private val biometricDataStore: BiometricDataStore
 ) : LocalServiceProvider {
     override fun getUserDataStore(): UserDataStore {
         return userDataStore
+    }
+
+    override fun getBiometric(): BiometricDataStore {
+        return biometricDataStore
     }
 }
